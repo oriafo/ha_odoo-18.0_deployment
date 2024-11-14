28db064bb8637392f1b737e23a204c5f2c654bbf
oriafo <abrahamibharunujele@gmail.com>
2024-11-14 03:39:55 +0100

    FIX: corrected the run_number syntax

--- infra/web-app-module/networking.tf
+++ infra/web-app-module/networking.tf
@@ -380,13 +380,13 @@ resource "aws_launch_template" "custom_lt" {
 #!/bin/bash -xe
 if [ github.head_ref == 'dev' ]; then
   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 681117582889.dkr.ecr.us-east-1.amazonaws.com
-  docker pull $REGISTRY/$REPOSITORY:$run_number
-  docker run -itd --name odoo-erp-$run_number -p 8069:8069 -e ODOO_USER=odoo  $REGISTRY/$REPOSITORY:$run_number 
+  docker pull $REGISTRY/$REPOSITORY:${var.run_number}
+  docker run -itd --name odoo-erp-${var.run_number} -p 8069:8069 -e ODOO_USER=odoo  $REGISTRY/$REPOSITORY:${var.run_number}
 else
   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 375410234341.dkr.ecr.us-east-1.amazonaws.com
-  docker $REGISTRY/$REPOSITORY:run_number 
-  docker run -itd --name odoo-erp-$run_number  -p 8069:8069 -e ODOO_USER=odoo 3$REGISTRY/$REPOSITORY:run_number 
-fi
+  docker $REGISTRY/$REPOSITORY:${var.run_number} 
+  docker run -itd --name odoo-erp-${var.run_number} -p 8069:8069 -e ODOO_USER=odoo 3$REGISTRY/$REPOSITORY:${var.run_number}
+fi  
 EOF
   )
 
