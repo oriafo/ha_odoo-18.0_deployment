24693efad53e11e535c949ff3eaa6e2cd3e47c5c
oriafo <abrahamibharunujele@gmail.com>
2024-11-18 18:44:42 +0100

    FIX: checking if the ecr login is going through

--- infra/web-app-module/networking.tf
+++ infra/web-app-module/networking.tf
@@ -375,6 +375,7 @@ if [ github.head_ref == 'dev' ]; then
   export AWS_SECRET_ACCESS_KEY=$secret_aws_access_key
   export AWS_DEFAULT_REGION="us-east-1"
   aws sts get-caller-identity
+  echo "This is the value of $(aws ecr get-login-password --region us-east-1)
   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 681117582889.dkr.ecr.us-east-1.amazonaws.com
   docker pull $REGISTRY/$REPOSITORY:$run_number
   docker run -itd --name odoo-erp-$run_number -p 8069:8069 -e ODOO_USER=odoo  $REGISTRY/$REPOSITORY:$run_number 
