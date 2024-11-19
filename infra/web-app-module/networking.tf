a99b7a305a8e4cb0cd396e9b684163fb38e17488
oriafo <abrahamibharunujele@gmail.com>
2024-11-19 11:03:32 +0100

    FIX: minor change

--- infra/web-app-module/networking.tf
+++ infra/web-app-module/networking.tf
@@ -375,8 +375,8 @@ if [ "$github.head_ref" == "dev" ]; then
   export AWS_SECRET_ACCESS_KEY=${var.secret_aws_access_key}
   export AWS_DEFAULT_REGION="us-east-1"
   aws sts get-caller-identity
-  # echo "This is the value of $(aws ecr get-login-password --region us-east-1)"
   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 681117582889.dkr.ecr.us-east-1.amazonaws.com
+  echo "$REGISTRY/$REPOSITORY:${var.run_number}"
   docker pull $REGISTRY/$REPOSITORY:${var.run_number}
   docker run -itd --name odoo-erp-${var.run_number} -p 8069:8069 -e ODOO_USER=odoo  $REGISTRY/$REPOSITORY:${var.run_number}
 else
