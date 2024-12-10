9197af23fc5123428828eb25f3e1ebe8229b134c
oriafo <abrahamibharunujele@gmail.com>
2024-12-10 22:40:03 +0100

    FIX: added the reconfigure flag to the terraform init command to reinitialize the backend

--- infra/web-app-module/compute.tf
+++ infra/web-app-module/compute.tf
@@ -17,139 +17,139 @@ resource "aws_instance" "k8_control_plane" {
   key_name              = var.key_pair
   vpc_security_group_ids = [aws_security_group.k8_master_sg.id]  
   
-#   user_data = base64encode(<<EOP  
-# #!/bin/bash -xe
-# #
-# # Setup for Control Plane (Master) servers
-# exec > /tmp/k8_control_output.log 2>&1 
-# sudo apt-get update
+  user_data = base64encode(<<EOP  
+#!/bin/bash -xe
+#
+# Setup for Control Plane (Master) servers
+exec > /tmp/k8_control_output.log 2>&1 
+sudo apt-get update
 
 
-# set -euxo pipefail
+set -euxo pipefail
 
-# # Kubernetes Variable Declaration
-# KUBERNETES_VERSION="v1.30"
-# CRIO_VERSION="v1.30"
-# KUBERNETES_INSTALL_VERSION="1.30.0-1.1"
+# Kubernetes Variable Declaration
+KUBERNETES_VERSION="v1.30"
+CRIO_VERSION="v1.30"
+KUBERNETES_INSTALL_VERSION="1.30.0-1.1"
 
-# # Disable swap
-# sudo swapoff -a
+# Disable swap
+sudo swapoff -a
 
-# # Keeps the swap off during reboot
-# (crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
-# sudo apt-get update -y
+# Keeps the swap off during reboot
+(crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
+sudo apt-get update -y
 
-# # Create the .conf file to load the modules at bootup
-# cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf 
-# overlay
-# br_netfilter
-# EOF
+# Create the .conf file to load the modules at bootup
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf 
+overlay
+br_netfilter
+EOF
 
-# sudo modprobe overlay
-# sudo modprobe br_netfilter
+sudo modprobe overlay
+sudo modprobe br_netfilter
 
-# # Sysctl params required by setup, params persist across reboots
-# cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-# net.bridge.bridge-nf-call-iptables  = 1
-# net.bridge.bridge-nf-call-ip6tables = 1
-# net.ipv4.ip_forward                 = 1
-# EOF
+# Sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
 
-# # Apply sysctl params without reboot
-# sudo sysctl --system
+# Apply sysctl params without reboot
+sudo sysctl --system
 
-# sudo apt-get update -y
-# sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
-# # Install CRI-O Runtime
-# sudo apt-get update -y
-# sudo apt-get install -y software-properties-common curl apt-transport-https ca-certificates
+# Install CRI-O Runtime
+sudo apt-get update -y
+sudo apt-get install -y software-properties-common curl apt-transport-https ca-certificates
 
-# curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/deb/Release.key |
-#     sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/deb/Release.key |
+    sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
 
-# echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/deb/ /" |
-#     sudo tee /etc/apt/sources.list.d/cri-o.list
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/deb/ /" |
+    sudo tee /etc/apt/sources.list.d/cri-o.list
 
-# sudo apt-get update -y
-# sudo apt-get install -y cri-o
+sudo apt-get update -y
+sudo apt-get install -y cri-o
 
-# sudo systemctl daemon-reload
-# sudo systemctl enable crio --now
-# sudo systemctl start crio.service
+sudo systemctl daemon-reload
+sudo systemctl enable crio --now
+sudo systemctl start crio.service
 
-# echo "CRI runtime installed successfully"
+echo "CRI runtime installed successfully"
 
-# # Install kubelet, kubectl, and kubeadm
-# curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key |
-#    sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# Install kubelet, kubectl, and kubeadm
+curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key |
+   sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-# echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" |
-#    sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" |
+   sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-# sudo apt-get update -y
-# sudo apt-get install -y kubelet="$KUBERNETES_INSTALL_VERSION" kubectl="$KUBERNETES_INSTALL_VERSION" kubeadm="$KUBERNETES_INSTALL_VERSION"
+sudo apt-get update -y
+sudo apt-get install -y kubelet="$KUBERNETES_INSTALL_VERSION" kubectl="$KUBERNETES_INSTALL_VERSION" kubeadm="$KUBERNETES_INSTALL_VERSION"
 
-# # Prevent automatic updates for kubelet, kubeadm, and kubectl
-# sudo apt-mark hold kubelet kubeadm kubectl
+# Prevent automatic updates for kubelet, kubeadm, and kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 
-# sudo apt-get update -y
+sudo apt-get update -y
 
-# # Install jq, a command-line JSON processor
-# sudo apt-get install -y jq
+# Install jq, a command-line JSON processor
+sudo apt-get install -y jq
 
-# # Retrieve the local IP address of the eth0 interface and set it for kubelet
-# local_ip="$(ip --json addr show ens5 | jq -r '.[0].addr_info[] | select(.family == "inet") | .local')"
+# Retrieve the local IP address of the eth0 interface and set it for kubelet
+local_ip="$(ip --json addr show ens5 | jq -r '.[0].addr_info[] | select(.family == "inet") | .local')"
 
-# # Write the local IP address to the kubelet default configuration file
-# cat > /etc/default/kubelet << EOF
-# KUBELET_EXTRA_ARGS=--node-ip=$local_ip
-# EOF
-
-
+# Write the local IP address to the kubelet default configuration file
+cat > /etc/default/kubelet << EOF
+KUBELET_EXTRA_ARGS=--node-ip=$local_ip
+EOF
 
 
-# set -euxo pipefail
 
-# # If you need public access to API server using the servers Public IP adress, change PUBLIC_IP_ACCESS to true.
 
-# PUBLIC_IP_ACCESS="false"
-# NODENAME=$(hostname -s)
-# POD_CIDR="192.168.0.0/16"
+set -euxo pipefail
 
-# # Pull required images
+# If you need public access to API server using the servers Public IP adress, change PUBLIC_IP_ACCESS to true.
 
-# sudo kubeadm config images pull
+PUBLIC_IP_ACCESS="false"
+NODENAME=$(hostname -s)
+POD_CIDR="192.168.0.0/16"
 
-# # Initialize kubeadm based on PUBLIC_IP_ACCESS
+# Pull required images
 
-# if [[ "$PUBLIC_IP_ACCESS" == "false" ]]; then
+sudo kubeadm config images pull
+
+# Initialize kubeadm based on PUBLIC_IP_ACCESS
+
+if [[ "$PUBLIC_IP_ACCESS" == "false" ]]; then
     
-#     MASTER_PRIVATE_IP=$(ip addr show ens5 | awk '/inet / {print $2}' | cut -d/ -f1)
-#     sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
+    MASTER_PRIVATE_IP=$(ip addr show ens5 | awk '/inet / {print $2}' | cut -d/ -f1)
+    sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
 
-# elif [[ "$PUBLIC_IP_ACCESS" == "true" ]]; then
+elif [[ "$PUBLIC_IP_ACCESS" == "true" ]]; then
 
-#     MASTER_PUBLIC_IP=$(curl ifconfig.me && echo "")
-#     sudo kubeadm init --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
+    MASTER_PUBLIC_IP=$(curl ifconfig.me && echo "")
+    sudo kubeadm init --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
 
-# else
-#     echo "Error: MASTER_PUBLIC_IP has an invalid value: $PUBLIC_IP_ACCESS"
-#     exit 1
-# fi
+else
+    echo "Error: MASTER_PUBLIC_IP has an invalid value: $PUBLIC_IP_ACCESS"
+    exit 1
+fi
 
-# # Configure kubeconfig
+# Configure kubeconfig
 
-# mkdir -p "$HOME"/.kube
-# sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
-# sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
+mkdir -p "$HOME"/.kube
+sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
+sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
-# # Install Claico Network Plugin Network 
+# Install Claico Network Plugin Network 
 
-# kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
-# EOP
-#   )
+EOP
+  )
 
   tags = {
     Name = "k8-master-${var.environment_name}-1"
