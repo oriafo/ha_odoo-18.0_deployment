bc6d33f85414f369662b8017f40f7639ae09d4b5
oriafo <abrahamibharunujele@gmail.com>
2024-12-10 18:59:17 +0100

    FIX: the correction to change my running instance to t3.medium wasnt effected because i did'nt push the variable.tf file

--- infra/web-app-module/compute.tf
+++ infra/web-app-module/compute.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "k8_control_plane" {
   key_name              = var.key_pair
   vpc_security_group_ids = [aws_security_group.k8_master_sg.id]  
   
-  user_data = base64encode(<<EOP
+  user_data = base64encode(<<EOP 
 #!/bin/bash -xe
 #
 # Setup for Control Plane (Master) servers
