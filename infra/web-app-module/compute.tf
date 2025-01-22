559f5b386c93d5e1d9305b1e54ae6554c17a8c2a
oriafo <abrahamibharunujele@gmail.com>
2025-01-22 18:50:29 +0100

    refactor: minor change

--- infra/web-app-module/compute.tf
+++ infra/web-app-module/compute.tf
@@ -141,13 +141,11 @@ else
     exit 1
 fi
 
-# Configure kubeconfig
-
+export HOME=/home/ubuntu
+echo "The home directory is: $HOME"
 mkdir -p "$HOME"/.kube
 sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
-
-# Install Claico Network Plugin Network 
 
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
